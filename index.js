57f113b362d9289594d0fce1a34d881f9d44a6cd
the-nise <64296151+the-nise@users.noreply.github.com>
2022-10-01 05:20:34 -0300

    New third-party modules

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
+const helmet = require('helmet')
 const Joi = require('joi')
+const logger = require('./logger')
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+app.use(helmet())
+app.use(logger)
+app.use(morgan('tiny'))
 
 const genres = [
     {id: 1, genre: "Horror"},
@@ -18,7 +26,6 @@ function validateGenre(genre) {
 
 function lookUpGenre(genres, req) {
     return genres.find(genre => genre.id === parseInt(req.params.id))
-    
 }
 
 app.post('/api/genres', (req, res) => {
